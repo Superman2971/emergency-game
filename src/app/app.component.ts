1cdf75c29536115cb5d64e5d9beb573ef84d8df7
Superman2971 <goldfarb.ian@gmail.com>
2017-10-21 15:58:44 -0700

    initial commit for emergency game project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  test = 'The only game which....blah blah blah.';
 }
