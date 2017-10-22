c628c43204317c92de4279806dd26deef8b1e16c
Superman2971 <goldfarb.ian@gmail.com>
2017-10-22 02:35:29 -0700

    work on progress and money

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BroadcasterService } from './services/broadcaster.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   activateRoom1 = false;
+  money = 3000;
+
+  constructor(private broadcast: BroadcasterService) {}
+
+  ngOnInit() {
+    // subscribe to 'money' changes
+    this.broadcast.on('money').subscribe(response => {
+      if (typeof response === 'number') {
+        this.money += response;
+      }
+    });
+  }
 }
--- src/app/room1/room1.component.ts
+++ src/app/room1/room1.component.ts
@@ -7,16 +7,40 @@ import { BroadcasterService } from './../services/broadcaster.service';
   styleUrls: ['./room1.component.scss']
 })
 export class Room1Component implements OnInit {
+  testing = this;
   sentText = 1;
+  buttons = {
+    active: true,
+    progress: 0,
+    progressInit: function() {
+      if (this.active) {
+        this.active = false;
+        this.progress = 100;
+        this.purchase();
+        const timer = setInterval(() => {
+          this.progress--;
+          if (this.progress === 0) {
+            clearInterval(timer);
+            this.active = true;
+          }
+        }, this.timeToActive / 100);
+      }
+    },
+    timeToActive: 1000,
+    purchase: function() {
+      console.log('purchase');
+      // this.broadcast.fire('money', -100);
+    }
+  };
 
   constructor(private broadcast: BroadcasterService) {}
 
   ngOnInit() {
-    this.broadcast.fire('newMessage', 'test run');
+    this.broadcast.fire('newMessage', 'A patient comes in asking for help.');
   }
 
-  sendMessage() {
-    this.broadcast.fire('newMessage', 'test run ' + this.sentText);
+  sendMessage(message) {
+    this.broadcast.fire('newMessage', message + ' ' + this.sentText);
     this.sentText++;
   }
 }
