83fcf219e524892ef7ddeb4e3efa8c0c04171a0c
Superman2971 <goldfarb.ian@gmail.com>
2017-10-23 21:58:55 -0700

    turned patients into objects and added timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,24 @@ import { BroadcasterService } from './services/broadcaster.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  time = 0;
   room1 = false;
   money = 3000;
-  patients = 0;
+  patients = [];
   room = 0;
+  conditions = [
+    'cold',
+    'fever',
+    'broken leg',
+    'head wound',
+    'internal bleeding',
+    'cancer'
+  ];
 
   constructor(private broadcast: BroadcasterService) {}
 
   ngOnInit() {
+    this.startClock();
     // subscribe to 'money' changes
     this.broadcast.on('money').subscribe(response => {
       if (typeof response === 'number') {
@@ -24,23 +34,44 @@ export class AppComponent implements OnInit {
     this.newPatient();
   }
 
-  newPatient() {
-    setTimeout(() => {
-      this.patients++;
-      if (this.patients < 10) {
-        this.newPatient();
-      }
-    }, this.randomTime(3000, 7000));
+  startClock() {
+    setInterval(() => {
+      this.time++;
+    }, 1000);
   }
 
-  randomTime(min, max) {
+  newPatient() {
+    setTimeout(() => {
+      this.patients.push({
+        condition: this.randomCondition(),
+        waitingTime: this.time,
+        diagnosed: false,
+        treated: false,
+        processed: false,
+        released: false,
+        alive: true
+      });
+      this.broadcast.fire('newMessage', 'A patient walks in with ' + this.patients[this.patients.length - 1].condition);
+      if (this.patients.length < 10) {
+        this.newPatient();
+        console.log(this.patients);
+      }
+    }, this.numberBetween(3000, 7000));
+  }
+
+  randomCondition() {
+    const selectedCondition = this.numberBetween(0, this.conditions.length - 1);
+    return this.conditions[selectedCondition];
+  }
+
+  numberBetween(min, max) {
     return Math.floor(Math.random() * max) + 1 + min;
   }
 
   roomChange(direction) {
     if (direction === 'back' && this.room > 0) {
       this.room--;
-    } else if (direction == 'forward' && this.room < 2) {
+    } else if (direction === 'forward' && this.room < 2) {
       this.room++;
     }
   }
